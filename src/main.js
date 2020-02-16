97ed0ba87315e8b0fb545db4ff04028f034a35b9
PaolaS2992 <huaripayta29.92@gmail.com>
2020-02-16 10:30:04 -0500

    Portafolio - sobre mi - contacto

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import changeView from './controller/route.js';
-
-const init = () => {
-  changeView(window.location.hash);
-  window.addEventListener('hashchange', () => changeView(window.location.hash));
-};
-
-window.addEventListener('load', init);
